94f8252a65332c13ef7a470332aaf41a02b74141
manjabogicevic <slavo@mimicom24.com>
2018-05-01 17:48:46 -0400

    return block

--- Python_Blockchain/blockchain.py
+++ Python_Blockchain/blockchain.py
@@ -2,3 +2,18 @@ import datetime
 import hashlib
 import json
 from flask import Flask, jsonify
+
+# Building Blockchain
+
+class Blockchain: 
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0') # Genessis Block
+
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+        'timestamp': str(datetime.datetime.now()),
+        'proof': proof,
+        'previous_hash': previous_hash}
+        self.chain.append(block)
+        return block
