e0d1277f7e3da4b6fcf28d2b2934f0f088b6ed98
manjabogicevic <slavo@mimicom24.com>
2018-05-12 13:14:28 -0400

    proof of work

--- Python_Blockchain/block.py
+++ Python_Blockchain/block.py
@@ -8,6 +8,7 @@ class Blockchain:
     def __init__(self):
         self.chain = []
         self.create_block(proof = 1, previous_hash='0')
+    
     def create_block(self, proof, previous_hash):
         block = {'index': len(self.chain) + 1, 'timestamp' = str(datetime.datetime.now()), 'proof': proof, 'previous_hash': previous_hash}
         self.chain.append(block)
@@ -15,4 +16,16 @@ class Blockchain:
 
     def get_previous_block(self):
         return self.chain[-1]
+
+    def proof_or_work(self, previous_proof):
+        new_proof = 1
+        check_proof: False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+            
     
