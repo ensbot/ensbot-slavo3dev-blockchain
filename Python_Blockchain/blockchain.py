35ffb121f3ca8c28178d6028fa29d091840b72be
manjabogicevic <slavo@mimicom24.com>
2018-05-12 12:15:45 -0400

    get_chain method

--- Python_Blockchain/blockchain.py
+++ Python_Blockchain/blockchain.py
@@ -59,7 +59,6 @@ app = Flask(__name__)
 blockchain = Blockchain()
 
 # mining blockchain
-
 @app.route('/mine_block', methods=['GET'])
 def mine_block():
     previous_block = blockchain.get_previous_block()
@@ -67,4 +66,15 @@ def mine_block():
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
     block = blockchain.create_block(proof, previous_hash)
-    response = {'message' = 'Congratulations, you just mined a block'}
+    response = {'message': 'Congratulations, you just mined a block',
+    'index': block['timestamp'],
+    'proof': block['proof'],
+    'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+# full Blockchain
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+    'length': len(blockchain.chian)}
+    return jsonify(response), 200
