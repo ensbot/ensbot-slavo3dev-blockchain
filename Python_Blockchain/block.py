0d9b5519e7f69c11444e4de7391f5fc5b58d654d
manjabogicevic <slavo@mimicom24.com>
2018-05-12 16:04:13 -0400

    ADDING A BLOCK

--- Python_Blockchain/block.py
+++ Python_Blockchain/block.py
@@ -10,7 +10,7 @@ class Blockchain:
         self.create_block(proof = 1, previous_hash='0')
     
     def create_block(self, proof, previous_hash):
-        block = {'index': len(self.chain) + 1, 'timestamp' = str(datetime.datetime.now()), 'proof': proof, 'previous_hash': previous_hash}
+        block = {'index': len(self.chain) + 1, 'timestamp':str(datetime.datetime.now()), 'proof': proof, 'previous_hash': previous_hash}
         self.chain.append(block)
         return block 
 
