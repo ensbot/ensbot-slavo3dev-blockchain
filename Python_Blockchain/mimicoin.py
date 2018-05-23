2cf356247622547c22067bb560dae9e3e57c28d1
slavo7 <slavo@mimicom24.com>
2018-05-23 10:39:16 -0400

    adding transaction method

--- Python_Blockchain/mimicoin.py
+++ Python_Blockchain/mimicoin.py
@@ -12,11 +12,15 @@ from urllib.parse import urlparse
 class Blockchain:
     def __init__(self):
         self.chain = []
+        self.transactions = []
         self.create_block(proof=1, previous_hash='0')
 
     def create_block(self, proof, previous_hash):
         block = {'index': len(self.chain) + 1, 'timestamp': str(
-            datetime.datetime.now()), 'proof': proof, 'previous_hash': previous_hash}
+            datetime.datetime.now()), 'proof': proof, 'previous_hash': previous_hash,
+            "transactions": self.transactions
+        }
+        self.transactions = []
         self.chain.append(block)
         return block
 
@@ -55,6 +59,10 @@ class Blockchain:
             previous_block = block
             block_index += 1
         return True
+
+    def add_transaction(self, sender, reciever, amount):
+        self.transactions.append(
+            {'sender': sender, 'reciever': reciever, 'amount': amount})
 
 
 # creating web app
