547c1512eaca174a3419a42d067d9511d2d62111
slavo7 <slavo@mimicom24.com>
2018-07-21 10:50:46 -0400

    adding smart contract

--- Smart_Contracts/bidder.sol
+++ Smart_Contracts/bidder.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.0; 
+ pragma solidity ^0.4.19; 
 
 contract Bidder {
     
@@ -7,5 +7,11 @@ contract Bidder {
      bool public  eligible;
      uint constant minBid = 1000;
      
-     
+     function setName(string nm) public {name = nm;}
+
+     function setBidAmount(uint x)public{bidAmount = x;}
+
+     function determinEligibility() public {
+         if(bidAmount >= minBid) eligible = true;
+         else eligible = false;}
 }
