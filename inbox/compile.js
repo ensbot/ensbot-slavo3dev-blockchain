c7d9329c7b9890e78478bdc6675102b3480c4529
slavo@mimicom24.com <slavo@mimicom24.com>
2018-09-03 19:57:12 -0400

    removing

--- inbox/compile.js
+++ inbox/compile.js
@@ -7,4 +7,4 @@ const inboxPath = path.resolve(__dirname, "contracts", "inbox.sol");
 const source = fs.readFileSync(inboxPath, 'utf8');
 
 // exporting module - inbox
-module.exports = solc.compile(source, 1)["Inbox"]
+module.exports = solc.compile(source, 1)["Inbox"];
