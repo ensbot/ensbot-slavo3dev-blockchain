eb27445c75391aa0a9c57e9cbefca78c5aa7ee72
slavo@mimicom24.com <slavo@mimicom24.com>
2018-09-03 19:50:55 -0400

    adding prettier

--- inbox/compile.js
+++ inbox/compile.js
@@ -6,8 +6,5 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, "contracts", "inbox.sol");
 const source = fs.readFileSync(inboxPath, 'utf8');
 
+// exporting module - inbox
 module.exports = solc.compile(source, 1)["Inbox"]
-
-
-
-
